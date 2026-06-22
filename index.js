a4a1d92db827fc4aafe358772dd3de388934d4a7
MB Playground <playground@masteringbackend.com>
2026-06-22 07:21:18 +0000

    autosave 08:21

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ ok: true, message: "Your backend is running 🎉" });
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "healthy" });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+console.log("This is good")
